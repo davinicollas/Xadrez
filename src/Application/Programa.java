1a422eeed7f80b64726d26121e9cfea2f63d85f4
davinicollas <davi.nicollas@hotmail.com>
2021-04-16 11:01:41 -0300

    Primeira classe

--- src/Application/Programa.java
+++ src/Application/Programa.java
@@ -1,9 +1,12 @@
 package Application;
 
+import Boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow");
+		Position position = new Position(4,3);
+		System.out.println(position);
 
 	}
 
