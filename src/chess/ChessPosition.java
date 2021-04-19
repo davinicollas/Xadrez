bbc34f7eb8c744519a46a310d728ec49bbec5fab
davinicollas <davi.nicollas@hotmail.com>
2021-04-19 10:46:11 -0300

    Criação do ChessException e ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,37 @@
+package chess;
+
+import Boardgame.Position;
+
+public class ChessPosition {
+	private char column;
+	private int row;
+
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Posição nao existe no tabuleiro");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+	public int getRow() {
+		return row;
+	}
+	
+	protected Position ToPosition() {
+		return new Position(8-row, column - 'a'); 
+	}
+	protected static ChessPosition fromPosition(Position ToPosition) {
+		return new ChessPosition((char)('a'- ToPosition.getColumn()), 8 - ToPosition.getRow());
+	}
+
+	@Override
+	public String toString() {
+		return "" + column +  row ;
+	}
+	
+
+}
